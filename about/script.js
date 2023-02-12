1ac786188f2b6d10fc9021be285f2119c4682d64
Suraj Singh <surajsinghdangi07@gmail.com>
2023-02-12 15:37:04 +0530

    Uploading

---
+++ about/script.js
@@ -0,0 +1,6 @@
+let menuBtn=document.getElementById("menu");
+let navbar = document.getElementById("navbar")
+
+menuBtn.addEventListener("click",function(){
+    navbar.classList.toggle("active")
+})
